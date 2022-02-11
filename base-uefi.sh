23d294523662e22fa7fb71d0a0c3231b275c8b8a
Eduardo Mosca <38006342+EduardoMosca@users.noreply.github.com>
2022-02-11 00:54:16 -0300

    dnsmasq

--- base-uefi.sh
+++ base-uefi.sh
@@ -19,6 +19,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable dhcpcd
+systemctl enable dnsmasq
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
